632a18124ffaf9e2a71c7904a649c220b1e2b4ee
hannan228 <abdulhannan1146@gmail.com>
2023-02-18 20:50:16 +0500

    Added home controller, workshop model with basic validation, added gem byebug, anotate, faker

---
+++ app/models/workshop.rb
@@ -0,0 +1,6 @@
+class Workshop < ApplicationRecord
+  validates :name, :description, presence:true
+  validates :start_date, :end_date, :start_date, :end_time, presence: true
+  validates :total_sits, :registration_fee, presence: true, numericality: true
+  validates :end_date, comparison: { greater_than: :start_date, message: 'cannot be before start date'}
+end
